12bd75c3b3e7aad279df056a5d3bc48d9175652d
Dani Muñoz <davimufo99@gmail.com>
2025-10-14 23:01:16 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+        public static void main(String[] args) {
+            String nombre = "Daniel";
+            int edad = 26;
+            String ciudad = "Madrid";
+
+            System.out.println("Mi nombre es: " +nombre);
+            System.out.println("Mi edad es: " +edad);
+            System.out.println("Vivo en: " +ciudad);
+
+        }
+}
